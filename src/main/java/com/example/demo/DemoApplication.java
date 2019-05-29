4cf8f07582aaaaf94a9c61049a2af4ece7817db8
ccf <602907455@qq.com>
2019-05-29 16:42:47 +0800

    i am dev

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DemoApplication.class, args);
         System.out.println(111);
+        System.out.println("i am dev");
     }
 
 }
